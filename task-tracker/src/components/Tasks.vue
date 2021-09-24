243a22ac728bfef5001854ab9d2f8671e5a95974
Sophie Raps <s.raps@onveda>
2021-09-24 16:44:02 +0200

    Added: well ... all

---
+++ task-tracker/src/components/Tasks.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="task in tasks" :key="task.id">
+      <Task
+        :task="task"
+        @toggle-reminder="$emit('toggle-reminder', task.id)"
+        @delete-task="$emit('delete-task', task.id)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+  emits: ["delete-task", "toggle-reminder"],
+};
+</script>
+
+<style scoped></style>
